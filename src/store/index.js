c6625d6c649968badfb5a00845ded65e7f4ecd5e
Willian Wagner <willianwagner@live.com>
2023-04-25 10:07:33 -0300

    novo vue

---
+++ src/store/index.js
@@ -0,0 +1,15 @@
+import { createStore } from 'vuex'
+
+export default createStore({
+  state: {
+    token:'a',
+    paginacao:[]
+  },
+  getters: {
+  },
+  mutations: {
+  },
+  actions: {
+  },
+ 
+})
