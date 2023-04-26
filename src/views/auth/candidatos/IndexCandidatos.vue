30180b14c934daf2efdab31c98882ac946167669
Willian Wagner <willianwagner@live.com>
2023-04-26 16:28:51 -0300

    Ajuste

--- src/views/auth/candidatos/IndexCandidatos.vue
+++ src/views/auth/candidatos/IndexCandidatos.vue
@@ -1,3 +1,6 @@
 <template>
+    <div class="d-flex p-2 btn__adicionar">
+        <a role="button"> <i class="fa fa-plus-circle" aria-hidden="true"></i> Adicionar</a>
+    </div>
     <h1>Cand</h1>
 </template>
